21f93184c503f4812c155799149576bb97b24e50
Tristyn Leos <tristyntech@gmail.com>
2018-04-02 07:06:13 -0500

    api working, starting on design

--- spotify-search/src/App.js
+++ spotify-search/src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+    this.runSearch = () => {
+      console.log('running')
+      const config = {
+        headers: {
+          "Authorization": "Bearer " + "BQB6GlldBP0LhenlfhphoHqYvFnhLb79P3Ih8E0yPhjgB0T21Ioxqwju0t3da4cUfmDt7YB6cq6UZw3wPiwhvk9n39OivmhZ8yUiy0Jy0lO3UnX57W5ZY0WRSzm78TQ45MbX55vfYBArWZAGxkAWzUSzy0_3XRQ"
+        }
+      }
+      fetch('https://api.spotify.com/v1/search?q=avenged&type=artist,track,album', config)
+      .then((results)=>{
+        return results.json();
+      })
+      .then((results) => {
+        console.log(results)
+      })
+      console.log('completed')
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="container" style={{height: 80, width: "100%", backgroundColor: 'blue'}}>
+
+        <div id='header-container'>
+          <div id='header'>
+
+            <div id="title-container">
+              <h1>Song Farm</h1>
+            </div>
+
+            <div id="search-container">
+              <h2>Search By:</h2>
+              <select>
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+              <input  />
+              <button>Search</button>
+            </div>
+
+          </div>
+        </div>
+
       </div>
     );
   }
